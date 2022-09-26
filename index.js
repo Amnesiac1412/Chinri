476c522c7a316c7b9d4c49ed83756e6e268cac23
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 20:38:49 +0400

    initialize github repository

---
+++ index.js
@@ -0,0 +1,24 @@
+const { messageLink } = require("discord.js")
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "Guilds",
+        "GuildMessages",
+        "MessageContent",
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello from the other side")
+    }
+    
+})
+
+client.login(process.env.TOKEN)
